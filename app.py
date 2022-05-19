66ec6e28e7dfd0d95018b49a95a7271a5f131afb
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2022-05-19 08:30:38 +0100

    fixed whitespace in Procfile

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ threadDB = mongo.db.threads
 userDB = mongo.db.users
 postDB = mongo.db.posts
 
-# a
+# aa
 @app.route("/", methods=['GET', 'POST'])
 def index():
     # if the route was called by the login page

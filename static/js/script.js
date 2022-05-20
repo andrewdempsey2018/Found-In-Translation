a7d6670af08d2c09d6c9e9e81382371aa6eea3d2
CristianB <buca.cristian@hotmail.com>
2022-05-20 23:30:29 +0100

    Add(script.js): signup validation rules

--- static/js/script.js
+++ static/js/script.js
@@ -8,3 +8,33 @@ $.validator.methods.email = function( value, element ) {
     return this.optional( element ) || /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test( value );
 };
 
+// Calls the validate method on the signup form
+$().ready(function() {
+    $('#signup_form').validate({
+        // Validation Rules
+        rules: {
+            email: {
+                required: true,
+                email: true,
+            },
+            username: {
+                required: true,
+                minlength: 4,
+            },
+            password: {
+                required: true,
+            },
+            password_conf: {
+                required: true,
+                equalTo: '#password',
+            },
+            terms: {
+                required,
+            },
+            language: {
+                required,
+            }
+
+        },
+
+})

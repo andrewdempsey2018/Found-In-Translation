c8ccc565075d75a4273e07007ed4cdfcd51f6569
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2022-05-22 14:40:50 +0100

    symbol svg reacts to selection

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -4,40 +4,31 @@
    all of your UX wishes!
 */
 
+/* Select the dropdown from the signup page */
 let dropdown = document.getElementById('language');
 
-//dropdown.length = 0;
-//let defaultOption = document.createElement('option');
-//defaultOption.text = 'no language selected';
-
-//dropdown.add(defaultOption);
-//dropdown.selectedIndex = 0;
-
+/* Get the list of ISO-639-1 languages & codes */
 const language_codes = language_list;
 
+/* Each element of the selection dropdown*/
 let dropdownItem = null;
-let image = null;
 
 for (let i = 0; i < language_codes.length; i++) {
 
+    /* Bootstrap dropdown expects the elements of be of type 'option' */
     dropdownItem = document.createElement('option');
-    //image = document.createElement('img');
 
-
-
-    //dropdownItem.className = 'dropdown-item';
+    /* Search the language codes for the relevant name */
     dropdownItem.textContent = language_codes[i].name;
+
+    /* Now get the appropriate ISO-639 code */
     dropdownItem.value = language_codes[i].code;
 
-
-    //image.src = "https://unpkg.com/language-icons/icons/" + language_codes[i].code + ".svg";
-    //image.width = 30;
-    //image.height = 30;
-    //dropdownItem.appendChild(image);
-
+    /* Add the option element to the dropdown list */
     dropdown.appendChild(dropdownItem);
 }
 
+/* Get the code and name from any dropdown element that is clicked */
 dropdown.addEventListener('change', event => {
-    console.log(event.target.value);
+    document.getElementById("symbol").src = "https://unpkg.com/language-icons/icons/" + event.target.value + ".svg"
 });

a6289b08678ae866a434fbe7add581dd28323f73
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2022-05-23 08:43:02 +0100

    avatars implemented

--- static/js/horscroll.js
+++ static/js/horscroll.js
@@ -5,7 +5,7 @@
 
 /* Select the dropdown from the signup page */
 let avatarselect = document.getElementById('carousel');
-
+console.log(avatarselect)
 /* Get the list of ISO-639-1 languages & codes */
 //const language_codes = language_list;
 
@@ -13,30 +13,39 @@ let avatarselect = document.getElementById('carousel');
 let avatarselectItem = null;
 let image = null;
 
-let imgurcodes = ["PVe0Rrj", "mmj6PGK", "Ig3gtUz", "34LbL0J", "gornF3o", "j1VdmLj", "LJJHG7w", "ceMTvvV"];
+let imgurcodes = ["PVe0Rrj", "mmj6PGK", "Ig3gtUz", "34LbL0J", "gornF3o", "j1VdmLj", "LJJHG7w", "ceMTvvV", "zJOBDqr", "roDWkKX", "EhJOV0E",
+    "hDbqrma", "pfUsXVU", "DUW9Laq", "oOzPthq", "t1nnGj9", "OF0xvVW", "g9132Pw", "1cYwGzK", "cztrLBD", "wlwDib1", "yZyoUCZ", "Q9HA3Hd", "OZbX2fo",
+    "YIh79Ya", "PsmudlJ", "cnIqGQc", "yK8Vzb7", "XIqjw2E", "hH7auGs", "fIsObzM", "z4Qbyip", "8yB5LaV", "aPkkaTR", "V65zySU", "unZPGYC", "1Uvifmb",
+    "db6Jp64", "Wz0Vqph", "OBmfZJx", "avz9FDZ", "rrwVXPg", "vY2QzTV", "t6aDWCA", "deX0CM2", "9YO8mfV", "lu9560a", "U0Elh9h", "21j7bnK", "Xe5nqSf"];
 
 for (let i = 0; i < imgurcodes.length; i++) {
 
     /* Bootstrap dropdown expects the elements of be of type 'option' */
-    avatarselectItem = document.createElement('carousel-item');
+    avatarselectItem = document.createElement('div');
+    avatarselectItem.classList.add("carousel-item")
 
-    /* Search the language codes for the relevant name */
-    avatarselectItem.className = "d-inline-block w-25";
+    if (i == 0) {
+        avatarselectItem.classList.add('active')
+    }
 
     /* Now get the appropriate ISO-639 code */
     image = document.createElement('img');
+    image.className = "d-inline-block"
+    image.classList.add("w-25")
     image.referrerPolicy = "no-referrer"
-    image.width = 20;
-    image.height = 20;
+    //image.width = 60;
+    //image.height = 60;
     image.src = "https://imgur.com/" + imgurcodes[i] + ".png";
 
-    avatarselectItem.appendChild(image);
+    avatarselectItem.appendChild(image)
+
+
 
     /* Add the option element to the dropdown list */
     avatarselect.appendChild(avatarselectItem);
 }
 
-/* Get the code and name from any dropdown element that is clicked */
-avatarselect.addEventListener('click', event => {
-    console.log("hello")
+$('.carousel').bind('slid.bs.carousel', function (e) {
+    var index = $(this).find('.active').index();
+    console.log(index);
 });
